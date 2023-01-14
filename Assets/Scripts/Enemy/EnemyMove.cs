30d4ac5b49c736a849ecb36486ed7f3646d7f477
TheLonestar1 <yura2000077@yandex.ru>
2023-01-15 03:31:21 +0700

    Add dogs and enity

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -9,6 +9,7 @@ public class EnemyMove : MonoBehaviour
     Rigidbody2D _rigidbody;
     Animator _anim;
     private StatsManager _statsManager;
+    [SerializeField] Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,15 @@ public class EnemyMove : MonoBehaviour
         if (_isRunnig)
         {
             _anim.SetBool("IsWalk", true);
-            _rigidbody.velocity = _statsManager.GetModifiedStat(StatType.Speed) * Vector2.left;
+            var heading = target.position - transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance;
+            direction.y = 0;
+            if (direction.x > 0)
+                transform.localScale = new Vector3(-0.9f, 0.925f, 1);
+            else
+                transform.localScale = new Vector3(0.9f, 0.925f, 1);
+            _rigidbody.velocity = _statsManager.GetModifiedStat(StatType.Speed) * direction;
         }
         else
         {
